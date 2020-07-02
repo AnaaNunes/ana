092803bd8f94ea60547a03b04ea3e98081c68046
VGANANUNES\VGAnaNunes <anaaribeiro7@gmail.com>
2020-07-02 11:12:05 +0100

    teste

--- src/main/java/br/ce/wcaquino/core/DSL.java
+++ src/main/java/br/ce/wcaquino/core/DSL.java
@@ -20,7 +20,7 @@ public class DSL {
 	
 	/********** TextField e TextArea  ***************/
 
-/*teste*/ 
+/*testse*/ 
 	public void escreve(By by, String texto) {
 		getDriver().findElements(by).sendKeys(texto);
 	}
